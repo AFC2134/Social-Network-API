ccaf204db131ab8bda8fa406d729d210f561d745
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-12-04 19:00:50 -0600

    thought model schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,31 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dataFormat');
+
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Please enter information about a thought!',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'Username required!',
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
