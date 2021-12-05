5aca1a4a7c5c39c8a2b8e54edf0c663bbb28b091
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-12-04 18:49:24 -0600

    new user model schema

--- models/User.js
+++ models/User.js
@@ -1,18 +1,39 @@
 const { Schema, model, Types } = require('mongoose');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: "Username required",
+            trim: true
+        },
+        email: {
+            type: String,
+            required: 'Email address required',
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
 
 
 
